1c59f91dd466470dc743446df3992360a21ec199
theorashid <theoaorashid@gmail.com>
2024-12-12 22:00:03 -0800

    add xarray and numba projects

--- readme.py
+++ readme.py
@@ -17,6 +17,8 @@ self_tree.add("[bold link=https://github.com/theorashid/probabilistic-programmin
 self_tree.add("[bold link=https://github.com/theorashid/mortality-statsmodel]mortality-statsmodel[/]                - [bright_black]scalable, Bayesian spatiotemporal models for mortality")
 self_tree.add("[bold link=https://github.com/sparklabnyc/bayesian-envhealth-models]bayesian-envhealth-models[/]           - [bright_black]Bayesian models for environmental health")
 self_tree.add("[bold link=https://github.com/sparklabnyc/cookiecutter-r-project]cookiecutter-r-project[/]              - [bright_black]template for analysis project in R")
+self_tree.add("[bold link=https://github.com/theorashid/numba-oslo]numba-oslo[/]                          - [bright_black]fractal simulation in numba")
+self_tree.add("[bold link=https://github.com/theorashid/xarray-tropical-cyclones]xarray-tropical-cyclones[/]            - [bright_black]tropical cyclone analysis with xarray")
 
 contrib_tree = tree.add("code I helped with", guide_style="bright_black")
 contrib_tree.add("[bold link=https://github.com/pyro-ppl/numpyro]numpyro[/]                             - [bright_black]probabilistic programming, jax backend")
@@ -25,7 +27,6 @@ contrib_tree.add("[bold link=https://github.com/pymc-devs/pytensor]pytensor[/]  
 contrib_tree.add("[bold link=https://github.com/jax-ml/bayeux]bayeux[/]                              - [bright_black]inference for Bayesian models in jax")
 contrib_tree.add("[bold link=https://github.com/thomaspinder/GPJax]GPJax[/]                               - [bright_black]Gaussian processes in jax")
 
-
 future_tree = tree.add("code I would like to work on", guide_style="bright_black")
 future_tree.add("[bold link=https://github.com/blackjax-devs/blackjax]blackjax[/]                            - [bright_black]samplers in jax")
 future_tree.add("[bold link=https://github.com/probml/dynamax]dynamax[/]                             - [bright_black]state space models in jax")
@@ -33,7 +34,7 @@ future_tree.add("[bold link=https://github.com/nimble-dev/nimble]nimble[/]      
 
 work_tree = tree.add("work", guide_style="bright_black")
 
-work_tree.add("Amazon                              - [bright_black]Applied Scientist, probabilistic forecasting")
+work_tree.add("Amazon                              - [bright_black]Applied Scientist II, probabilistic forecasting")
 work_tree.add("Faculty                             - [bright_black]Data Science Fellow")
 work_tree.add("Freelance                           - [bold link=https://www.loamin.com/]Loamin[/], [bold link=https://sparklabnyc.github.io/site/home.html]Robbie Parks, Columbia[/], [bold link=https://sethrf.com/]Seth Flaxman, Oxford[/]")
 work_tree.add("Amazon                              - [bright_black]Applied Scientist intern")
